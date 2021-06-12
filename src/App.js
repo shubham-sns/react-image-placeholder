b86e646ec7263c31d10d1ce082137270643ed5bd
Shubham Sananse <shubhamsananse9@gmail.com>
2021-06-12 14:10:24 +0530

    feat: added react-img-placeholder published library fix: readme typo fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { Image } from './components/image';
+// import { Image } from './components/image';
+import { Image } from 'react-img-placeholder';
 
 const src = 'https://i.redd.it/2ghbw4ea28471.jpg';
 
@@ -8,12 +9,7 @@ function App() {
     <div className="App">
       {/* <img src={src} alt="logo" width="200" /> */}
 
-      <Image
-        style={{ borderRadius: '50%' }}
-        src={src}
-        height="200"
-        width="200"
-      />
+      <Image src={src} height="200" width="200" />
     </div>
   );
 }
